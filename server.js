f4d4400c3a18cee26cf35e0240a50d8aaa9811ee
quocthai99 <quocthai5101999@gmail.comn>
2022-10-13 12:50:17 +0700

    start

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express, { urlencoded } from 'express'
 require('dotenv').config()
 import cors from 'cors'
+import initRoutes from './src/routes'
 
 const app = express()
 app.use(cors({
@@ -10,7 +11,7 @@ app.use(cors({
 app.use(express.json())
 app.use(express(urlencoded({extended: true})))
 
-app.use('/', (req, res) => {res.send('server on ..')})
+initRoutes(app)
 
 const port = process.env.PORT || 8888
 const listener = app.listen(port, () => {
